29ef1ea4b7ab9271fcd0e01094c06f8d05805022
Naoya Yamashita <conao3@gmail.com>
2023-08-10 00:28:23 +0900

    use argparse

--- src/pydantic_argparse/__main__.py
+++ src/pydantic_argparse/__main__.py
@@ -1,2 +1,14 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--foo', type=int, default=42)
+    parser.add_argument('--bar')
+
+    return parser.parse_args()
+
+
 def main():
-    print('hello')
+    args = parse_args()
+    print(args)
